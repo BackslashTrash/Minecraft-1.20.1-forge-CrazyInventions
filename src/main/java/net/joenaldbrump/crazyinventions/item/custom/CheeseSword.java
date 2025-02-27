4ed96f1c655afb100a35d4eec9789a9ed0172687
Jeff's homework <jiaweit417@gmail.com>
2025-02-27 14:13:07 -0500

    added cheese sword and stuff

--- src/main/java/net/joenaldbrump/crazyinventions/item/custom/CheeseSword.java
+++ src/main/java/net/joenaldbrump/crazyinventions/item/custom/CheeseSword.java
@@ -1,5 +1,11 @@
 package net.joenaldbrump.crazyinventions.item.custom;
 
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 
@@ -7,5 +13,15 @@ public class CheeseSword extends SwordItem {
     public CheeseSword(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
     }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        pStack.hurtAndBreak(1, pAttacker, (p_43296_) -> {
+            p_43296_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+        });
+        pTarget.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 2));
+        return true;
+    }
+
 
 }
